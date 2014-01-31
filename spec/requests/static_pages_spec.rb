27d7e18c63b165a6578082886d1fc7f0a5fcd1cc
John Duffie <johnduffie91@gmail.com>
2014-01-31 09:15:20 -0500

    add common test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,20 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
+  
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading)    { 'FlexNet' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
